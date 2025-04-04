b8cb734381d92e98e6bcf66f71a39bcd8cca5dab
mauriciolaser <mauriciocastrovaldez@gmail.com>
2025-04-04 16:00:47 +0300

    enlace UTM para parking

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer">
     Todos los derechos reservados.{' '}
-    <a href="https://www.mirageconsultores.com" target="_blank" rel="noopener noreferrer">
+    <a
+      href="https://www.mirageconsultores.com/?utm_source=zoomlion.com.pe&utm_medium=referral&utm_campaign=redirect_parking"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       Mirage Consultores.
     </a>
   </footer>
