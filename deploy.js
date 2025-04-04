c8d80b50bfdfc387215a0214fe1f28d141458cae
mauriciolaser <mauriciocastrovaldez@gmail.com>
2025-04-04 16:03:27 +0300

    deploy en config

--- deploy.js
+++ deploy.js
@@ -1,26 +1,26 @@
-import 'dotenv/config'; // Carga automáticamente las variables de entorno
 import FtpDeploy from 'ftp-deploy';
 import { resolve } from 'path';
+import configVars from './deploy-config.js'; // Importa las variables desde el archivo
 
 // Imprime las variables de entorno para depuración
-console.log('FTP_HOST:', process.env.FTP_HOST);
-console.log('FTP_USER:', process.env.FTP_USER);
-console.log('FTP_PASSWORD:', process.env.FTP_PASSWORD);
-console.log('FTP_PORT:', process.env.FTP_PORT);
-console.log('FTP_REMOTE_PATH:', process.env.FTP_REMOTE_PATH);
+console.log('FTP_HOST:', configVars.FTP_HOST);
+console.log('FTP_USER:', configVars.FTP_USER);
+console.log('FTP_PASSWORD:', configVars.FTP_PASSWORD);
+console.log('FTP_PORT:', configVars.FTP_PORT);
+console.log('FTP_REMOTE_PATH:', configVars.FTP_REMOTE_PATH);
 
 const ftpDeploy = new FtpDeploy();
 
 const config = {
-  user: process.env.FTP_USER,
-  password: process.env.FTP_PASSWORD,
-  host: process.env.FTP_HOST,
-  port: process.env.FTP_PORT || 21,
-  localRoot: resolve('./dist'), // Ruta local del build
-  remoteRoot: process.env.FTP_REMOTE_PATH, // Ruta remota en el servidor FTP
-  include: ['*', '**/*'], // Incluir todos los archivos y subdirectorios
-  deleteRemote: false, // No borrar archivos remotos que ya no existan localmente (ajusta si lo requieres)
-  forcePasv: true // Usar modo PASV
+  user: configVars.FTP_USER,
+  password: configVars.FTP_PASSWORD,
+  host: configVars.FTP_HOST,
+  port: configVars.FTP_PORT || 21,
+  localRoot: resolve('./dist'),
+  remoteRoot: configVars.FTP_REMOTE_PATH,
+  include: ['*', '**/*'],
+  deleteRemote: false,
+  forcePasv: true
 };
 
 console.log('Iniciando deploy vía FTP...');
